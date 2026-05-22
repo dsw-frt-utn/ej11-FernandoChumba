dab5506963b21827c0e04602668ddd749b74a440
Fernando Chumba <fernando.chumba@alu.frt.utn.edu.ar>
2026-05-22 20:17:03 -0300

    resolucion ejercicio 11

--- Dsw2026Ej11/Collections/CasoDictionary.cs
+++ Dsw2026Ej11/Collections/CasoDictionary.cs
@@ -1,10 +1,50 @@
-﻿namespace Dsw2026Ej11.Collections;
+﻿using Dsw2026Ej11.Domain;
+
+namespace Dsw2026Ej11.Collections;
 
 //Crear un diccionario donde la clave sea el legajo y el valor el alumno
 //Incluir un método para agregar un alumno al diccionario
 //Incluir un método para buscar un alumno utilizando la clave
 //Incluir un método para retornar el diccionario
 //Incluir un método para eliminar un alumno utilizando la clave
+
+
 public class CasoDictionary
 {
+    //clave
+    private Dictionary<int, Alumno> _diccionarioAlumnos = new Dictionary<int, Alumno>();
+
+    //agregar alumno
+    public void AgregarAlumno(int legajo , Alumno alumno)
+    {
+        _diccionarioAlumnos[legajo] = alumno;
+    }
+
+    //buscar alumno
+    public Alumno buscarAlumno(int leggajo)
+    {
+        if (_diccionarioAlumnos.ContainsKey(leggajo))
+        {
+            return _diccionarioAlumnos[leggajo];
+
+        }
+        return null;
+
+    }
+
+    //obtener diccionario
+    public Dictionary<int, Alumno> obtenerDiccionario()
+    {
+        return _diccionarioAlumnos;
+    }
+
+    //eliminar alumno
+    public void eliminarAlumno(int legajo)
+    {
+        if (_diccionarioAlumnos.ContainsKey(legajo))
+        {
+            _diccionarioAlumnos.Remove(legajo);
+        }
+    }
+
 }
--- Dsw2026Ej11/Collections/CasoList.cs
+++ Dsw2026Ej11/Collections/CasoList.cs
@@ -1,4 +1,6 @@
-﻿namespace Dsw2026Ej11.Collections;
+﻿using Dsw2026Ej11.Domain;
+
+namespace Dsw2026Ej11.Collections;
 
 //Crear un campo que represente una lista de alumnos (List<>)
 //Incluir un método para agregar alumnos a la lista
@@ -8,4 +10,43 @@
 //Incluir un método para eliminar un alumno en una determinada posición de la lista
 public class CasoList
 {
+    private List<Alumno> _listaAlumnos = new List<Alumno>();
+
+    //agregar alumno
+    public void AgregarAlumno(Alumno alumno)
+    {
+        _listaAlumnos.Add(alumno);
+    }
+
+    //retornar lista 
+    public List<Alumno> ObtenerLista()
+    {
+        return _listaAlumnos;
+    }
+    //buscar alumno por nombre
+
+    public Alumno? BuscarAlumno(string nombre)
+    {
+        return _listaAlumnos.FirstOrDefault(alumno => alumno.Nombre == nombre);
+    }
+
+    //eliminar alumno
+    public void EliminarAlumno(Alumno alumno)
+    {
+        _listaAlumnos.Remove(alumno);
+    }
+
+    //eliminar alumno por posicion
+    public void EliminarAlumnoPorPosicion(int posicion)
+    {
+        if (posicion >= 0 && posicion < _listaAlumnos.Count)
+        {
+            _listaAlumnos.RemoveAt(posicion);
+        }
+    }
+
+
+
+
+
 }
